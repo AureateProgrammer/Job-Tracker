4ef074aad3ad2dee2a6ccc1c75b374af99f67507
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-06-10 11:05:21 -0400

    Creating files

--- src/Component/JobName.tsx
+++ src/Component/JobName.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-
 export function JobName() {
   return (
     <div className="jobname">
       <h1>Job Tracker</h1>
+      <h2>Track your job applications and stay organized!</h2>
+      <ul className = "ul1">
+        <li>Keep track of your job applications in one place.</li>
+        <li>Set reminders for follow-ups and interviews.</li>
+        <li>Organize your job search with ease.</li>
+      </ul>
+      
     </div>
+    
   )
 }
 
